937aeb5274a7c08988b56216ebae71e7af59b31c
Lucas Tavares <lucas.s_tavares@hotmail.com>
2020-12-15 19:17:26 -0300

    refatoração e adicionando cenarios

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,18 +1,97 @@
 
 describe 'Forms de Login' do
 
-    it 'Login com Email e Senha validos' do
+    before(:all) do
 
-        visit "https://training-wheels-protocol.herokuapp.com/login"
-        page.windows[0].maximize
+        #urls de teste
+        @url_pos_login = 'https://training-wheels-protocol.herokuapp.com/secure'
+        @url_pre_login = 'https://training-wheels-protocol.herokuapp.com/login'
 
-        fill_in 'userId', with: 'stark'
-        fill_in 'password', with: 'jarvis!'
+        #mensagens para validacao
+        @msg_login_sucesso = 'Olá, Tony Stark. Você acessou a área logada!'
+        @msg_erro_email = 'O usuário informado não está cadastrado!'
+        @msg_erro_senha = 'Senha é invalida!'
 
-        click_button 'Login'
-
-        puts find('#flash').text
-        expect(find('#flash').visible?).to be true
+        #massa de teste
+        @email_valido = 'stark'
+        @senha_valida = 'jarvis!'
+        @email_invalido = 'lucas'
+        @senha_invalida = '112233'
 
     end
+
+    it 'Login com Email e Senha validos' do
+
+        #Acessando tela de Login 
+        visit @url_pre_login
+        page.windows[0].maximize
+
+        #Realizando login
+        fill_in 'userId', with: @email_valido
+        fill_in 'password', with: @senha_valida
+        click_button 'Login'
+
+        #Validações pós Login
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash').text).to include @msg_login_sucesso
+        
+        expect(page.current_url).to eq @url_pos_login
+    end
+
+    it 'Login com Email inválido' do
+
+        #Acessando tela de Login 
+        visit @url_pre_login
+        page.windows[0].maximize
+
+        #Realizando login
+        fill_in 'userId', with: @email_invalido
+        fill_in 'password', with: @senha_valida
+        click_button 'Login'
+
+        #Validações pós Login
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash').text).to include @msg_erro_email
+        
+        expect(page.current_url).to eq @url_pre_login
+
+    end
+
+    it 'Login com Email válido e Senha inválida' do
+
+        #Acessando tela de Login 
+        visit @url_pre_login
+        page.windows[0].maximize
+
+        #Realizando login
+        fill_in 'userId', with: @email_valido
+        fill_in 'password', with: @senha_invalida
+        click_button 'Login'
+
+        #Validações pós Login
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash').text).to include @msg_erro_senha
+        
+        expect(page.current_url).to eq @url_pre_login
+
+    end 
+
+    it 'Login com Email e Senha em branco' do
+
+        #Acessando tela de Login 
+        visit @url_pre_login
+        page.windows[0].maximize
+
+        #Realizando login
+        fill_in 'password', with: ''
+        click_button 'Login'
+
+        #Validações pós Login
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash').text).to include @msg_erro_email
+        
+        expect(page.current_url).to eq @url_pre_login
+
+    end 
+
 end
